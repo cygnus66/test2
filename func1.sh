4b4a7b20ba349f962321b093215f9993a1df4c92
Cloud User <centos@ip-172-31-31-112.ap-southeast-1.compute.internal>
2020-03-09 06:39:04 +0000

    v1.1

---
+++ func1.sh
@@ -0,0 +1,2 @@
+echo "Func1: func1.sh version 1.0"
+echo "Func1: This is code in func1".
--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
-echo "Version 1.0"
-echo "This is my code line 1".
+echo "Main: Version 1.1"
+echo "Main: This is my code line 1".
+echo "Main: add calling func1.sh"
+./func1.sh
+echo "Main: after calling func1.sh"
